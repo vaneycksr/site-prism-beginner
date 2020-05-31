2d4fb168592f92b96e87b730e0c2e0a5fab8a06a
vaneycksr <vaneyck23@gmail.com>
2020-05-30 22:43:07 -0300

    criando arquivos yml para gerenciar executar os testes em varios ambientes

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,13 +9,23 @@ require 'site_prism'
 # carrega o arquivo que criei
 require_relative 'page_helper.rb'
 
+# cria uma constante para os diferentes ambientes (homolog, prod, testes)
+AMBIENTE = ENV['AMBIENTE'] # homolog
+
+# carrega o arquivo yml, que pode ser qualquer ambiente (homolog, prod, testes)
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTE}.yml")
+
+# vai carregar o arquivo homolog.yml
+
 # define o modulo que criei como global
 World(PageObjects)
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
 
-  config.app_host = 'https://automacaocombatista.herokuapp.com'
+  #  config.app_host = 'https://automacaocombatista.herokuapp.com'
+
+  config.app_host = CONFIG['url_padrao']
 
   config.default_max_wait_time = 5
 end
